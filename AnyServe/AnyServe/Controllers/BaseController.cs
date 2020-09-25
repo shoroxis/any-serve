d5a3cc75f93d24489de3bbc5ef4b9f67fe13991b
Ilya Smirnov <shor@oxis.me>
2020-09-25 17:29:46 +0300

    Add logger

--- AnyServe/AnyServe/Controllers/BaseController.cs
+++ AnyServe/AnyServe/Controllers/BaseController.cs
@@ -1,5 +1,6 @@
 ﻿using AnyServe.Storage;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -15,10 +16,12 @@ namespace AnyServe.Controllers
     public class BaseController<T> : Controller where T : class
     {
         private Storage<T> _storage;
+        private readonly ILogger<BaseController<T>> _logger;
 
         public BaseController(Storage<T> storage)
         {
             _storage = storage;
+            _logger.LogInformation("BaseController created");
         }
 
         [HttpGet]
