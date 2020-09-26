dfdbe3dfab008b203a6d29c636075d4c5e42552b
Ilya Smirnov <shor@oxis.me>
2020-09-26 12:32:53 +0300

    Add must interface for all models

---
+++ AnyServe/AnyServe/Models/IBaseModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AnyServe.Models
+{
+    interface IBaseModel
+    {
+        Guid id { get; set; }
+    }
+}
